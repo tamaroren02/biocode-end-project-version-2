bd5b4ee7780af9bb0f9b431b4e5428be41086710
tamaroren02 <tamaroren0211@gmail.com>
2026-05-18 15:00:50 +0300

    הוספת החלק שיערה כתבה לפונקציה is conserved שמוציא 1 או 0

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
-def is_conserved(amino_list):#מיפוי תכונות של חומצות אמינו
+def is_conserved(amino_list):
+    #מיפוי תכונות של חומצות אמינו
     amino_groups = [['R','H','K'],['D','E'],['S', 'T', 'N', 'Q'],['P', 'C', 'G'],['A', 'V', 'I','L', 'M', 'F','Y', 'W']]
     
     counter_list = [0, 0, 0, 0, 0, 0]
@@ -16,3 +17,18 @@ def is_conserved(amino_list):#מיפוי תכונות של חומצות אמינ
     #5
     Hydrophobic_sidechains = ['A', 'V', 'I','L', 'M', 'F','Y', 'W']
     """
+    #מחזירה 0/1 (שמור או לא שמור)
+    for i in range(len(amino_list)):
+        curr_acid = amino_list[i]
+
+        for h in range(len(amino_groups)):
+            for j in range(len(amino_groups[h])):
+                if curr_acid == amino_groups[h][j]:
+                    counter_list[h] += 1
+
+    for i in range(len(counter_list)):
+        if counter_list[i] >= 4:
+            return 1
+    return 0   
+
+
