72be3ceb12f54a09dfd755859e1a89b9a69a32dd
YaaraPer <yaaraperlmuter@gmail.com>
2026-05-23 16:10:21 +0300

    תיקנתי את הפונקציות compare ו-amino_acid_groups

--- scripts/main.py
+++ scripts/main.py
@@ -7,31 +7,15 @@ def seq_to_string(seq):#הופך את הליסט לסטרינג
 #------------------------------------------------
 
 def is_conserved(amino_acid_list):
-    #מיפוי תכונות של חומצות אמינו
-    #amino_groups = [['R','H','K'],['D','E'],['S', 'T', 'N', 'Q'],
-    #                ['P', 'C', 'G'],['A', 'V', 'I','L', 'M', 'F','Y', 'W']]
-    
-    #counter_list = [0, 0, 0, 0, 0]
-    
-    """
-    #1
-    Electric_charged_sidechains_P = ['R','H','K']
-    #2
-    Electric_charged_sidechains_N = ['D','E']
-    #3
-    Electric_polar_uncharged_sidechains = ['S', 'T', 'N', 'Q']
-    #4
-    special_cases = ['P', 'C', 'G']
-    #5
-    Hydrophobic_sidechains = ['A', 'V', 'I','L', 'M', 'F','Y', 'W']
-    """
-    
     cnt = 0
-    for i in range(amino_acid_list):
+    for i in range(len(amino_acid_list)):
         if amino_acid_list[i] == "-":
             cnt += 1
 
-    if (len(set(amino_acid_list)) <= 2 or cnt <=3):
+    if cnt >= 3:
+        return '-'
+
+    if len(set(amino_acid_list)) <= 2:
         return 1
     else:
         return 0
@@ -83,61 +67,77 @@ def amino_acid_groups(original_seq, mutated_seq):
 
     for i in range(len(original_seq)):
         if original_seq[i] != mutated_seq[i]:
-            for j in range(groups):
+            for j in range(len(groups)):
                 if mutated_seq[i] in groups[j] and original_seq[i] in groups[j]:
                     cnt += 1
     return cnt
 
 #------------------------------------------------
-def compare(zero_and_one_list, animle_list):
-    conserved_cnt = 0
-    len_conserved, start1, stop1 = max_seq(zero_and_one_list, 1)
-    conserved_seq_original = animle_list[start1: stop1]
-    num_mutation1 = int(len_conserved * 0.2)
-    conserved_seq_mutated = Mutate_protein(conserved_seq_original, num_mutation1)
-    
-    
-    conserved_cnt = amino_acid_groups(conserved_seq_original, conserved_seq_mutated) == False
-            
-    # חישוב בכמה אחוזים הרצף ישתנה
-    conserved_percentage = 100 * (len_conserved / conserved_cnt)
 
-    non_conserved_cnt = 0
-    len_non_conserved, start0, stop0 = max_seq(zero_and_one_list, 0)
-    non_conserved_seq_original = animle_list[start0: stop0]
-    num_mutation0 = int(len_non_conserved * 0.2)
-    non_conserved_seq_mutated = Mutate_protein(non_conserved_seq_original, num_mutation0)
-
-    
-    non_conserved_cnt = amino_acid_groups(non_conserved_seq_original, non_conserved_seq_mutated) == False
-            
-    # חישוב בכמה אחוזים הרצף ישתנה
-    non_conserved_percentage = 100 * (len_non_conserved / non_conserved_cnt)
-
-    return conserved_percentage, non_conserved_percentage
-
-#------------------------------------------------
 def Mutate_protein(seq, num_mutation):#מקבלת רצף חומצות אמינו של חיה ומחזירה את אותו רצף רק עם מוטצות נוקדתיות של החלפה
   '''
   '''
-  for i in range(num_mutation):
-    amini_acids_list = ['F', 'S', 'Y', 'C', 'L', '*', 'W',
+  amino_acids_list = ['F', 'S', 'Y', 'C', 'L', '*', 'W',
                         'P', 'H', 'R', 'Q',
                         'I', 'T', 'N', 'K', 'M',
                         'V', 'A', 'D', 'G', 'E']
 
-    rand_acid = random.choice(amini_acids_list)
+  for i in range(num_mutation):
+    
+    
+    rand_acid = random.choice(amino_acids_list)
+    #print("rand_acid", rand_acid)
     rand_num = random.randrange(0,len(seq))
-    
-    if seq[rand_num] != rand_acid:
-        change_protein = seq[0:rand_num]+ rand_acid + seq[(rand_num+1):]
-    
-    else:
-        amini_acids_list.remove(rand_acid)
-        rand_acid = random.choice(amini_acids_list)
-        change_protein = seq[0:rand_num]+ rand_acid + seq[(rand_num+1):]
+    #print("seq[rand_num]", seq[rand_num])
 
-  return change_protein
+    while seq[rand_num] == rand_acid:
+        rand_num = random.randrange(0,len(seq))
+    
+    seq = seq[0:rand_num]+ rand_acid + seq[(rand_num+1):]
+    #print("seq", seq)
+    
+  return seq
+
+#------------------------------------------------
+def compare(zero_and_one_list, animle_list):
+    
+    len_conserved, start1, stop1 = max_seq(1, zero_and_one_list)
+    conserved_seq_original = animle_list[start1: stop1 + 1]
+
+    #print("conserved_seq_original", conserved_seq_original)
+
+    num_mutation1 = int(len_conserved * 0.2)
+    conserved_mutated_seq = Mutate_protein(conserved_seq_original, num_mutation1)
+    
+    conserved_cnt = amino_acid_groups(conserved_seq_original, conserved_mutated_seq)
+
+    print ("conserved_cnt", conserved_cnt)
+    if len_conserved != 0 :
+        # חישוב בכמה אחוזים הרצף ישתנה
+        conserved_percentage = 100 * ( conserved_cnt / len_conserved)
+    # print(conserved_percentage)
+    else:
+        conserved_percentage = 0
+
+
+    len_non_conserved, start0, stop0 = max_seq(0, zero_and_one_list)
+    non_conserved_seq_original = animle_list[start0: stop0 + 1]
+
+    num_mutation0 = int(len_non_conserved * 0.2)
+    non_conserved_seq_mutated = Mutate_protein(non_conserved_seq_original, num_mutation0)
+
+    non_conserved_cnt = amino_acid_groups(non_conserved_seq_original, non_conserved_seq_mutated)
+
+    if len_non_conserved != 0:
+        # חישוב בכמה אחוזים הרצף ישתנה
+        non_conserved_percentage = 100 * ( non_conserved_cnt / len_non_conserved)
+        
+    else:
+        non_conserved_percentage = 0
+
+
+    return conserved_percentage, non_conserved_percentage
+
 #------------------------------------------------
 def file_to_list(file):
     curr_seq = ""
@@ -160,6 +160,20 @@ def file_to_list(file):
     
     return seq_list
 #------------------------------------------------
+def position(protein_list):
+    zero_one_list = []
+    for i in range(len(protein_list[0])):
+        position_list = []
+        for h in range(len(protein_list)):
+            position_list.append((protein_list[h])[i])
+
+        zero_one_list.append(is_conserved(position_list))
+
+    return zero_one_list
+
+
+#------------------------------------------------
+
 def conserved_list(seq_str):#
     one_zero_list=[]
     seq_length=len(seq_str)
@@ -199,15 +213,35 @@ RBP1_file = open('data/RBP1_MSA.fasta', 'r')
 #הגדרת שתי רשימות המכילות את רצפי החלבונים שבקצים, כל רשימה מייצגת קובץ אחר.
 GAPDH_list = []
 RBP1_list = []
+GAPDH_conserved = 0
+GAPDH_non_conserved = 0
+
 
 #ליסט של שתי הקבצים במקום קובץ
 GAPDH_list = file_to_list(GAPDH_file)
-RBP1_list = file_to_list(RBP1_file)
+zero_one_GAPDH_list = position(GAPDH_list)
+#print (zero_one_GAPDH_list)
+GAPDH_conserved, GAPDH_non_conserved = compare(zero_one_GAPDH_list, GAPDH_list[0])
+print("GAPDH_conserved", GAPDH_conserved)
+print("GAPDH_non_conserved", GAPDH_non_conserved)
 
-#העברת כל אחד מהרצפים בקבצים 70 מוטציות
+
+RBP1_list = file_to_list(RBP1_file)
+zero_one_RBP1_list = position(RBP1_list)
+#print (zero_one_RBP1_list)
+
+
+
+
+
+
+
+
+
+'''#העברת כל אחד מהרצפים בקבצים 70 מוטציות
 mutated_GAPDH_list =mutate_ustage(GAPDH_list)
 mutated_RBP1_list=mutate_ustage(RBP1_list)
-
+'''
 
 """
 def ustage_conserved_list(organizam_list):
@@ -222,7 +256,7 @@ print(ustage_conserved_list(mutated_GAPDH_list))
 """
 
 
-
+'''
 GAPDH_list_conserved=conserved_list(GAPDH_list)
 mutated_GAPDH_list_conserved=conserved_list(mutated_GAPDH_list)
 RBP1_list_conserved=conserved_list(RBP1_list)
@@ -233,10 +267,4 @@ mutated_RBP1_list_conserved=conserved_list(mutated_RBP1_list)
 comapred_GAPDH_list=compare(GAPDH_list_conserved, mutated_GAPDH_list_conserved)
 comapred_RBP1_list=compare(RBP1_list_conserved, mutated_RBP1_list_conserved)
 
-
-
-x = [0,0,0,1,0,1,1,1,0,1,1,1,1,1,1]
-max, sta, sto = max_seq(1, x)
-print(max)
-print(sta)
-print(sto)
+'''
